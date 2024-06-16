92d1ed856a5c480362cb660855a826a207415d50
katabami83 <katabami0141@gmail.com>
2024-06-16 23:31:17 +0900

    html test

---
+++ script.js
@@ -0,0 +1,6 @@
+import pIdolList from './class/PIdolList.js';
+document.addEventListener('DOMContentLoaded', () => {
+    const main_character_select = document.getElementById('main-character');
+    const sub_character_select  = document.getElementById('sub-character');
+    console.log(pIdolList)
+}, false);
