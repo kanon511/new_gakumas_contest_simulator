a0f0eddb3b78e1ab41bf74ff185668d6bf91bb96
katabami83 <katabami0141@gmail.com>
2024-08-04 01:25:36 +0900

    init commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <Simulation />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import Simulation from './components/Simulation.vue'
+</script>
