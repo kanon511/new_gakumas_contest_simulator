eb04f3d7f15bfd0934160c04190a9ede2faed18a
katabami83 <katabami0141@gmail.com>
2024-08-15 02:47:37 +0900

    [change] Change evaluation parameter proposed in Issue #5

--- scripts/simulator/class/Calculator.js
+++ scripts/simulator/class/Calculator.js
@@ -33,9 +33,18 @@ export class Calculator {
             return unitValue * args[0] * Math.sqrt(status.remainTurn+5) * 0.75;
         }
         if (type == 'block') {
-            if (args[0] < 0) {
-                return unitValue * args[0] / (status.hp + status.block) * status.remainTurn * 9;
-            }
+            // if (args[0] < 0) {
+            //     return unitValue * args[0] / (status.hp + status.block) * status.remainTurn * 9;
+            // }
+            //
+
+            // Issue #5: やる気システムのAIロジックを変更(Proposed by kanon511)
+            if (args[0] < 0) { 
+                if (status.remainTurn<2){ 
+                    return 15 * args[0]; //重写 
+                } 
+                return 15 * args[0] * status.remainTurn *status.remainTurn / 4; //重写 
+            } 
             return unitValue * args[0] * Math.sqrt(status.remainTurn+5) * 0.75;
         }
         if (type == 'score') {
