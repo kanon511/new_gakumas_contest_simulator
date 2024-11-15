b5dc9ee1d93822788a8ef5162a98d0748d38e377
katabami83 <katabami0141@gmail.com>
2024-11-16 02:24:51 +0900

    [fix] vite.config

--- vite.config.mjs
+++ vite.config.mjs
@@ -11,6 +11,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/gakumas_contest_simulator/' : './',
   plugins: [
     VueRouter(),
     Vue({
